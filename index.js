2c48f7a3a6f9fdb3a1b684dee00fa22313806db1
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-05-17 17:02:13 +0530

    Reading the json data using fs module of node.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+console.log('Start!!');
+
+//adding fs module
+const fileSystem = require('fs');
+/*
+Node.js includes fs module to access physical file system. The fs module is responsible for all the asynchronous or synchronous file I/O operations.
+*/
+// console.log(__dirname); will give current path 
+const json = fileSystem.readFileSync(`${__dirname}/data/data.json`,`utf-8`);
+const laptopData = JSON.parse(json); // converts to JSON object
+console.log(laptopData);
